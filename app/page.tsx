a630d9c6f268eb8cc82d89f4ee06787db64a4783
SakataRyuichi <ryuichi.design@gmail.com>
2025-03-22 15:16:41 +0900

    メインページコンポーネントを追加

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import AudioVisualizer from '@/app/components/AudioVisualizer';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <AudioVisualizer />
+    </main>
+  );
+}
